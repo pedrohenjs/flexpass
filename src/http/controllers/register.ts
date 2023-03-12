7ee170936361f858593cf1b6eabbd73a8ef11412
pedrohenjs <pedrojs.contato@gmail.com>
2023-03-12 04:13:30 -0300

    Create user register service

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,4 @@
-import { prisma } from '@/lib/prisma'
-import { hash } from 'bcryptjs'
+import { registerService } from '@/services/register'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -15,25 +14,15 @@ export async function registerController(
 
   const { email, name, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+  try {
+    await registerService({
       email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    reply.status(409).send()
-  }
-
-  await prisma.user.create({
-    data: {
       name,
-      email,
-      password_hash,
-    },
-  })
+      password,
+    })
+  } catch {
+    return reply.status(409).send()
+  }
 
   return reply.status(201).send()
 }
